cb23505bcb9e9134d8bd67c27f2d54f5a1a6c33e
Easer <liu.easer@qq.com>
2019-09-22 19:07:51 +0800

    demo

---
+++ pages/imageSource.js
@@ -0,0 +1,11 @@
+export default{
+  defaultGroup:require('./images/defaultGroup.png'),
+  defaultMeeting:require('./images/defaultMeeting.png'),
+  defaultPersonal:require('./images/defaultPersonal.png'),
+  headerbg:require('./images/headerbg.png'),
+  joinMeet:require('./images/joinMeet.png'),
+  manageMeet:require('./images/manageMeet.png'),
+  startMeet:require('./images/startMeet.png'),
+  unstar:require('./images/unstar.png'),
+  star:require('./images/star.png'),
+}
